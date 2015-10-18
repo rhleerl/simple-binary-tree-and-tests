c5d067fb7fde87b0c9230c3ca764ce6b434a6c50
rhleerl <rhleerl@gmail.com>
2015-10-18 12:47:40 -0700

    tidy up, place junit jar files into a lib dir

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 
-javac -cp .:junit-4.12.jar:hamcrest-core-1.3.jar org/rhlee/*.java
+javac -cp .:./lib/junit-4.12.jar:./lib/hamcrest-core-1.3.jar org/rhlee/*.java
--- run-test-suite.sh
+++ run-test-suite.sh
@@ -1,2 +1,2 @@
 
-java -cp .:junit-4.12.jar:hamcrest-core-1.3.jar org.junit.runner.JUnitCore org.rhlee.TestSuite
+java -cp .:./lib/junit-4.12.jar:./lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore org.rhlee.TestSuite
